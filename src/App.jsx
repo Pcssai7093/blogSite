97fc1624b6551ec1ad68b7d5d087879de4de848d
raja <pcssai7093@gmail.com>
2025-01-30 00:10:01 +0530

    added mui navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import AddBlog from './pages/AddBlog'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import AddBlog from "./pages/AddBlog/AddBlog";
+import Navbar from "./pages/Navbar/Navbar";
 
 function App() {
-
   return (
     <>
-      <AddBlog />
+      <Navbar />
+      {/* <Navbar /> */}
+      {/* navbar will be out of router and the page content changes */}
+      <div>
+        <AddBlog />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
