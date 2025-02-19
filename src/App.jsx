c5f0ee4703d437b0ba5625c240df8ccaa46c3b96
raja <pcssai7093@gmail.com>
2025-02-19 17:42:41 +0530

    bugfix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import AddBlog from "./pages/AddBlog/AddBlog";
 import Navbar from "./pages/Navbar/Navbar";
