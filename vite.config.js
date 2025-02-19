22ecc6d3c5cf5d81fa7802972653dccf4c017396
raja <pcssai7093@gmail.com>
2025-02-19 21:36:30 +0530

    bugfix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/blogSite/", // 👈 Set this to match your repo name
+  base: "/", // 👈 Set this to match your repo name
   // build: {
   //   outDir: "dist", // Ensures Vite builds to dist/
   // },
