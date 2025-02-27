1fc73b426af2182da41d847f168bd22cded15ef1
raja <pcssai7093@gmail.com>
2025-02-27 21:50:58 +0530

    env commit

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -2,13 +2,20 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MSG_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
-  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+  // apiKey: import.meta.env.VITE_API_KEY,
+  // authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  // projectId: import.meta.env.VITE_PROJECT_ID,
+  // storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  // messagingSenderId: import.meta.env.VITE_MSG_SENDER_ID,
+  // appId: import.meta.env.VITE_APP_ID,
+  // measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+  apiKey: "AIzaSyA8zmxkrVOQASNo3GrOgkr0M2YpnLirfcw",
+  authDomain: "blogapp-81127.firebaseapp.com",
+  projectId: "blogapp-81127",
+  storageBucket: "blogapp-81127.firebasestorage.app",
+  messagingSenderId: "115255025425",
+  appId: "1:115255025425:web:504535798c62a1b95ab6b0",
+  measurementId: "G-M1051GH37G",
 };
 
 // Initialize Firebase
